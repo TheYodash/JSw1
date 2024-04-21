6bab43713dae274206e054a997869e0c16fcf0c3
Moayad Mohammed <moayad.ashabi@gmail.com>
2024-04-21 20:21:08 +0200

    Adding Cohort 48 Exercises

---
+++ Practice/cohort48jsexercises.js
@@ -0,0 +1,76 @@
+// /* 
+//     Exercise 1: Calculate the Sum of Array Elements:
+//     Write a function calculateSum that takes an array of numbers as input and returns the sum of all the elements in the array.
+// */
+
+// const totalNumbers = [6, 7, 8, 6, 6, 6, 9];
+
+
+// function calculateSum(totalNumbers){
+//     let index = 0;
+//     let totalSum = 0;
+//     while (index < totalNumbers.length){
+        
+//         totalSum = totalSum + totalNumbers[index];
+//         index++;
+//     }
+
+//     console.log("The total sum is ", totalSum);
+// }
+
+// calculateSum(totalNumbers);
+
+// /* 
+//     Exercise 2: Calculate the Average of Array Elements:
+//     Write a function calculateAverage that takes an array of numbers as input and returns the average of all the elements in the array.
+// */
+
+// const averageNumbers = [6, 3, 48, 4, 12, 8, 16];
+
+// function calculateAverage(totalNumbers){
+//     let index = 0;
+//     let totalSum = 0;
+//     while (index < totalNumbers.length){
+        
+//         totalSum = totalSum + totalNumbers[index];
+//         index++;
+//     }
+
+//     console.log("The Average is ", totalSum/index);
+// }
+
+// calculateAverage(totalNumbers);
+
+
+// /*
+//     Exercise 3: Find the Largest Number in an Array:
+//     Write a function findLargestNumber that takes an array of numbers as input and returns the largest number in the array.
+// */
+
+// const largestNumbers = [3, 7, 9, 48, 6, 6, 18];
+
+// let index = 0;
+//     let highestNumber = largestNumbers[index];
+    
+//     while (index < largestNumbers.length-1){
+//         if(highestNumber<largestNumbers[index+1]){
+//             highestNumber = largestNumbers[index+1];
+//         }   
+//         index++;
+        
+//     }
+
+//     console.log("The largest number is ", highestNumber);
+
+
+    // let x = "";
+    // console.log(typeof x);
+    // console.log(x);
+
+if(1 ==1 ){
+    const x = 20;
+}
+
+console.log(x);
+
+
